eccdca7fdc4272afa3461a87d591dc6844b5a3b0
Bhawesh <bhaweshjainskype@gmail.com>
2024-05-15 17:31:23 +0530

    Fixed SideMenu

---
+++ app/transactions/page.jsx
@@ -0,0 +1,9 @@
+const Transactions = () => {
+    return (
+        <div className="flex flex-col gap-2">
+            <div>Transactions</div>
+        </div>
+    )
+}
+
+export default Transactions
---
+++ app/ui/nav/nav.jsx
@@ -0,0 +1,28 @@
+"use client"
+import { useState } from "react";
+import Sidebar from "../sidebar/sidebar";
+import Logo from "../logo/logo";
+import { MdMenu } from "react-icons/md";
+
+const Nav = () => {
+    const [openMenu, setOpenMenu] = useState(false)
+
+    let handleMenuClick = () => {
+        setOpenMenu(!openMenu)
+    }
+
+    return (
+        <div>
+            <div className="md:hidden fixed w-full bg-white">
+                <div className=" flex flex-row justify-between items-center p-4">
+                    <Logo />
+                    <MdMenu size={25} className="" onClick={handleMenuClick} />
+                </div>
+            </div>
+
+            <Sidebar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+        </div>
+    )
+}
+
+export default Nav
