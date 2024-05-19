8d77896458fdb7aaab7d3a36b30c16289f8ef1a1
Bhawesh Jain <bhaweshjain384@gmail.com>
2024-05-19 23:47:51 +0530

    Withdraw body

--- app/dashboard/withdraw/page.jsx
+++ app/dashboard/withdraw/page.jsx
@@ -1,37 +1,15 @@
-import Transaction from "@/app/ui/Transaction/transaction";
-import WithdrawItem from "@/app/ui/Withdraw/withdrawItem";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001"
+import WithdrawBody from "./withdrawBody/withdrawBody";
 
-const Withdraw = async () => { 
 
-    const res = await fetch(`${baseUrl}/api/dashboard/withdraw`, {
-        method: "GET",
-        cache: "no-store",
-        headers: {
-            "Content-type": "application/json"
-        }
-    });
-
-    let list = []
-
-    if (res.ok) {
-        let body = await res.json()
-
-        list = body.data
-    }
+const Withdraw = () => { 
 
     return (
         <div className="flex flex-col items-center px-2">
             <h2 className="text-2xl font-bold my-5">Withdraw</h2>
 
             <div className="flex flex-col items-center overflow-y-auto w-full">
-                {
-                    list.map(item => (
-                        <WithdrawItem key={item._id} item={item} />
-                    ))
-                }
-
+                <WithdrawBody />
             </div>
         </div>
     )
